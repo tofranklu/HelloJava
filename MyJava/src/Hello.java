3a175b12f4cee8cd3766941a79c590f48dcd6deb
FRank <tofranklu@hotmail.com>
2019-11-16 15:46:09 +0800

    add netbeans project files

---
+++ MyJava/src/Hello.java
@@ -0,0 +1,27 @@
+public class Hello {
+    /**
+     * 測試參數
+     * @param args 
+     */
+    
+    public static void main(String[] args) {
+//        boolean b = false;
+//        System.out.print("b=");
+//        System.out.println(b);
+        
+        System.out.println("b=" + 123);
+        
+        float f;
+        f = 1 + 3.14F;
+        f = 3L + 3.14F;
+        double x = 1/2f;
+        System.out.println("1/2 = " + x);
+        
+        int a = 1;
+        int b;
+        b = ++a;
+        System.out.println("abcdefg");
+        System.out.println("b = " + b);
+       
+    }
+}
---
+++ MyJava/src/NewClass.java
@@ -0,0 +1,6 @@
+
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("NewClass Java");   //sout加tab鍵 會自動展開
+    }    
+}
